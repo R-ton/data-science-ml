0c585d3318f1d11753e4748c181b4c29bffc4c4f
Antonio <antonio.ratsimbazafy@yahoo.com>
2024-01-25 18:37:59 +0300

    Assign legend and plot the double figure one time only

--- Linear Regression/main.py
+++ Linear Regression/main.py
@@ -4,7 +4,7 @@ from linear_regression import LinearRegression
 from data_process import read_data_file
 from sklearn.model_selection import train_test_split
 from matplotlib.pyplot import plot, grid, title, xlabel, ylabel, scatter, show, figure, legend
-import matplotlib.pyplot as pp
+
 def exp_simple_linear_regression(X,y):
     model_line_reg = LinearRegression(True)
     X_train, X_test, y_train,  y_test = train_test_split(X, y, test_size=0.25)
@@ -13,7 +13,7 @@ def exp_simple_linear_regression(X,y):
     y_train_predict = model_line_reg.predict(X_train)
     
     #Plotting
-    figure('Test Dataset')
+    figure('Simple linear regression')
     # Scatter plot the test data, using green color, with specified line widths and label
     scatter(X_test,y_test, c='g', linewidths=2, label='Trained PER for FG')
     # Plot the test data and the predicted values, using a specific color and label
@@ -21,8 +21,8 @@ def exp_simple_linear_regression(X,y):
     xlabel('Actual FG%')
     ylabel('Predicted PER')
     title('Actual FG% vs Predicted PER')
+    legend()
 
-    show()
 
 def exp_multiple_linear_regression(X, y):
     model_line_reg = LinearRegression(True)
@@ -32,33 +32,18 @@ def exp_multiple_linear_regression(X, y):
     y_train_predict = model_line_reg.predict(X_train)
 
     # Plotting
-    figure(figsize=(12, 6))
+    figure('Multiple linear regression', figsize=(12, 6))
     # Create a scatter plot using y_test as x values and y_test_predict as y values
-    scatter(y_test, y_test_predict)
+    scatter(y_test, y_test_predict, label='Predicted Test')
     # Plot a line from the minimum to the maximum values of y_test and y_test_predict
-    plot([y_test.min(), y_test.max()], [y_test_predict.min(), y_test_predict.max()], 'k--', lw=2)
+    plot([y_test.min(), y_test.max()], [y_test_predict.min(), y_test_predict.max()], 'k--', lw=2, label='best fit')
     xlabel('Actual Player in Dataset Index')
     ylabel('Predicted  Player in Dataset Index')
     title('Actual vs Predicted player in DataSet Index')
-
-    # # Plotting the first feature against the target
-    # ax1 = fig.add_subplot(121)
-    # ax1.scatter(X_test[:, 1], y_test, label='Actual APG')
-    # ax1.scatter(X_test[:, 1], y_test_predict, label='Predicted PER')
-    # ax1.set_xlabel('APG')
-    # ax1.set_ylabel('PER')
-    # ax1.legend()
-
-    # # Plotting the second feature against the target
-    # ax2 = fig.add_subplot(122)
-    # ax2.scatter(X_test[:, 2], y_test, label='Actual FG%')
-    # ax2.scatter(X_test[:, 2], y_test_predict, label='Predicted PER')
-    # ax2.set_xlabel('FG%')
-    # ax2.set_ylabel('PER')
-    # ax2.legend()
+    legend(loc='best')
 
 
-    show()
+
    
 if __name__ == "__main__":
     data = read_data_file()
@@ -68,7 +53,9 @@ if __name__ == "__main__":
     y_simple_target = data['PER'].values
 
     #predict the target PER for a feature FG%
-    # exp_simple_linear_regression(X=X_simple_feature, y=y_simple_target)
+    exp_simple_linear_regression(X=X_simple_feature, y=y_simple_target)
 
     #predict the player based on PPG','RPG', 'APG', 'FG%', 'PER
     exp_multiple_linear_regression(X=X_multi_feature, y=y_multi_target)
+
+    show()
